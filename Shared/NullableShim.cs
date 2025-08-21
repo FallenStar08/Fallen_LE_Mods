cce076cd4372247b1a9554ed4c6e6a8205803c09
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2025-08-21 21:28:11 +0200

    appease this dumbass compiler

---
+++ Shared/NullableShim.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.All)]
+    internal sealed class NullableAttribute : Attribute
+    {
+        public NullableAttribute(byte b) { }
+        public NullableAttribute(byte[] b) { }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method)]
+    internal sealed class NullableContextAttribute : Attribute
+    {
+        public NullableContextAttribute(byte flag) { }
+    }
+}
