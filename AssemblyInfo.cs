999bb50027c5be380e39967b6780bf1c719e3188
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-04-04 16:48:35 +0200

    Make this even more janky

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,11 @@
+using System.Reflection;
 using Fallen_LE_Mods;
 using MelonLoader;
 
+#if IMPROVED_TOOLTIPS
 [assembly: MelonInfo(typeof(MyMod), "FallenStar's Improved Tooltips", "3.1.2", "FallenStar")]
-[assembly: MelonGame("Eleventh Hour Games", "Last Epoch")]
+#elif AUTO_ENABLER
+[assembly: MelonInfo(typeof(MyMod), "FallenStar's Auto Enabler", "1.0.0", "FallenStar")]
+#else
+[assembly: MelonInfo(typeof(MyMod), "FallenStar's LE Mods", "1.0.0", "FallenStar")]
+#endif
