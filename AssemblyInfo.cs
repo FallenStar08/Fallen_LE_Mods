c580a838e2745d590c56dc159b722ba86b8358a3
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-04-04 17:11:07 +0200

    use BuildInfo as source of truth cause I'm lazy

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,11 +1,4 @@
-using System.Reflection;
 using Fallen_LE_Mods;
 using MelonLoader;
 
-#if IMPROVED_TOOLTIPS
-[assembly: MelonInfo(typeof(MyMod), "FallenStar's Improved Tooltips", "3.1.2", "FallenStar")]
-#elif AUTO_ENABLER
-[assembly: MelonInfo(typeof(MyMod), "FallenStar's Auto Enabler", "1.0.0", "FallenStar")]
-#else
-[assembly: MelonInfo(typeof(MyMod), "FallenStar's LE Mods", "1.0.0", "FallenStar")]
-#endif
+[assembly: MelonInfo(typeof(MyMod), Fallen_LE_Mods.BuildInfo.Name, Fallen_LE_Mods.BuildInfo.Version, Fallen_LE_Mods.BuildInfo.Author)]
---
+++ BuildInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Fallen_LE_Mods
+{
+    public static class BuildInfo
+    {
+        public const string Author = "FallenStar";
+
+#if IMPROVED_TOOLTIPS
+        public const string Name = "FallenStar's Improved Tooltips";
+        public const string Version = "3.1.3";
+        public const string MainClass = "MyMod";
+#elif AUTO_ENABLER
+        public const string Name = "FallenStar's Auto Enabler";
+        public const string Version = "1.0.0";
+        public const string MainClass = "MyMod";
+#else
+        public const string Name = "FallenStar's LE Mods";
+        public const string Version = "6.6.6";
+        public const string MainClass = "MyMod";
+#endif
+    }
+}
