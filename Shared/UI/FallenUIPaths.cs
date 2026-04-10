fd6b2e4f254af2f1f806c85fd99e6989a0c322dd
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-04-10 04:50:28 +0200

    start centralizing object paths

---
+++ Shared/UI/FallenUIPaths.cs
@@ -0,0 +1,27 @@
+﻿namespace Fallen_LE_Mods.Shared.UI
+{
+    public static class FallenUIPaths
+    {
+        public static class GenericScrollBar
+        {
+            public const string Root = "GUI/Panel System/Panel Stacks/Left Panel Stack/Settings Panel(Clone)/Content/Sound/Scrollbar";
+        }
+        public static class SoundTab
+        {
+            public const string Root = "GUI/Panel System/Panel Stacks/Left Panel Stack/Settings Panel(Clone)/Content/Sound/Viewport/Sound-container";
+        }
+        public static class Stash
+        {
+            public const string Root = "GUI/Panel System/Panel Pool/StashPanelExpandable(Clone)";
+            public const string SearchBox = Root + "/left-container/SearchBox";
+        }
+
+        public static class Observatory
+        {
+            public const string Root = "GUI/Panel System/Panel Stacks/Full Screen Panel Stack/Observatory(Clone)";
+            public const string Config = Root + "/ObservatoryConfig";
+        }
+    }
+
+
+}
