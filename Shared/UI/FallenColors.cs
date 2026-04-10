b48d25558f23f832cdb8e8b102fb64764f9d3365
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-04-10 04:49:20 +0200

    colors

---
+++ Shared/UI/FallenColors.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Fallen_LE_Mods.Shared.UI
+{
+    public static class FallenColors
+    {
+        public static readonly Color HeaderCyan = new(0.1f, 0.8f, 1f, 1f);
+        public static readonly Color SuccessGreen = new(0.2f, 1f, 0.2f);
+        public static readonly Color ErrorRed = new(1f, 0.2f, 0.2f);
+        public static readonly Color CoolGold = new(1f, 0.84f, 0f, 1f);
+        public static readonly Color GhostWhite = new(1f, 1f, 1f, 0.8f);
+    }
+
+}
