a7d8b423797d07f2786ad2a71c6fc5d1670e9e39
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-04-17 02:28:07 +0200

    Coroutine Helpers (delayed calls)

---
+++ Shared/CoroutineHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using MelonLoader;
+using UnityEngine;
+
+namespace Fallen_LE_Mods.Shared
+{
+    public static class CoroutineHelper
+    {
+        public static void DelayFixed(Action action)
+        {
+            MelonCoroutines.Start(WaitFixed(action));
+        }
+
+        public static void DelayFrames(int frames, Action action)
+        {
+            MelonCoroutines.Start(WaitFrames(frames, action));
+        }
+
+        public static void DelayMillis(float ms, Action action)
+        {
+            MelonCoroutines.Start(WaitSeconds(ms / 1000f, action));
+        }
+
+        private static IEnumerator WaitFixed(Action action)
+        {
+            yield return new WaitForFixedUpdate();
+            action?.Invoke();
+        }
+
+        private static IEnumerator WaitFrames(int frames, Action action)
+        {
+            for (int i = 0; i < frames; i++)
+            {
+                yield return null;
+            }
+            action?.Invoke();
+        }
+
+        private static IEnumerator WaitSeconds(float seconds, Action action)
+        {
+            yield return new WaitForSeconds(seconds);
+            action?.Invoke();
+        }
+    }
+}
